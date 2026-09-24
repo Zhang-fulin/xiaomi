24baf0f401ec38066c4537871c6ae391b6d6e5fa
Zhang-fulin <49267133+Zhang-fulin@users.noreply.github.com>
2026-09-24 14:24:23 +0800

    Update default cost price and share count

--- src/constants.ts
+++ src/constants.ts
@@ -3,10 +3,10 @@
 // ========================
 
 /** 买入成本 (HKD) */
-export const DEFAULT_COST_PRICE = 42.396;
+export const DEFAULT_COST_PRICE = 42.100;
 
 /** 持股数 (股) */
-export const DEFAULT_SHARE_COUNT = 26000;
+export const DEFAULT_SHARE_COUNT = 26400;
 
 /** 月定投 (RMB) */
 export const DEFAULT_MONTHLY_INVEST = 10000;
